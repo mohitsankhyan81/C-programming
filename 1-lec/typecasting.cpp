e50cc49dbd5e4634344fb49ce081acc5a23afbbb
Mohit <mohitsankhyan253@gmail.com>
2025-07-05 16:51:32 +0530

    type casting

---
+++ 1-lec/typecasting.cpp
@@ -0,0 +1,24 @@
+// implicit function
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char grade='A';
+//     int Number= grade;
+//     cout<<Number<<endl;
+//     return 0;
+// }
+
+
+// explicit
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    double roll=100.45;
+    int newroll=(int)roll;
+    cout<<newroll<<endl;
+    return 0;
+}
