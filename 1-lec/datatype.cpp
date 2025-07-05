0fa2a9e66baac17eb703c265349501a16fcaf54f
Mohit <mohitsankhyan253@gmail.com>
2025-07-05 16:40:35 +0530

    data type

---
+++ 1-lec/datatype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age =28;
+    cout << age <<endl;
+    char name='A';
+    cout << name <<endl;
+    float number=1.12;
+    cout<<number<<endl;
+    bool name1=true;
+    cout<<name1<<endl;
+    return 0;
+}
