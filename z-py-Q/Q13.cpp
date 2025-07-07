15046bcda84eea960673f71d43e0afe117f7e226
Mohit <mohitsankhyan253@gmail.com>
2025-07-07 20:30:56 +0530

    important

---
+++ z-py-Q/Q13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int calc(int n){
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+int ncr(int n, int r){
+    int fac1 = calc(n);
+    int denor = calc(r) * calc(n - r);
+    return fac1 / denor;
+}
+
+int main(){
+    int n = 8, r = 2;
+    int res = ncr(n, r);
+    cout << "ans is " << res << endl;
+    return 0;
+}
