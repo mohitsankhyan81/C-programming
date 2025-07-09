423deee454ca40cd33acead4eaf5cf35ff13857e
Mohit <mohitsankhyan253@gmail.com>
2025-07-09 19:14:30 +0530

    local scope

---
+++ 6-lec/socpe/local-scope.cpp
@@ -0,0 +1,22 @@
+//  local scope is use for local variable see bellow with example
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     if(3>1){
+//         int x=1; // x is local variable
+//     }
+//     cout<<"x is "<<x<<endl;// we can't print local varible like that
+// }
+
+// one more example of local scope variable
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<10;i++){
+        int x=1;//local variable 
+        //cout<<"x is"<<x<<endl; this is correct way of printing local variable
+    }
+    cout<<"x is"<<x<<endl;//we can print local variable like that
+}
