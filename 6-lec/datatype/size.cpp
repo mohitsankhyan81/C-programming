bf5248a87c08c0bb548c951669720c1a15d1b6d6
Mohit <mohitsankhyan253@gmail.com>
2025-07-09 19:31:04 +0530

    size of data type

---
+++ 6-lec/datatype/size.cpp
@@ -0,0 +1,14 @@
+// size of data types in memory
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"size of int "<<sizeof(int)<<endl;
+    cout<<"size of char "<<sizeof(char)<<endl;
+    cout<<"size of float "<<sizeof(float)<<endl;
+    cout<<"size of long "<<sizeof(long)<<endl;
+    cout<<"size of int long "<<sizeof(int long)<<endl;
+    cout<<"size of short "<<sizeof(short)<<endl;
+    cout<<"size of long lomg "<<sizeof(long long)<<endl;
+    cout<<"size of string "<<sizeof(string)<<endl;
+}
