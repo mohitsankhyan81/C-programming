dd78d6e95250e4cde96901303f2dc3b0e7ff6fd8
Mohit <mohitsankhyan253@gmail.com>
2025-07-09 19:14:56 +0530

    local scope

--- 6-lec/socpe/local-scope.cpp
+++ 6-lec/socpe/local-scope.cpp
@@ -11,12 +11,12 @@
 
 // one more example of local scope variable
 
-#include <iostream>
-using namespace std;
-int main(){
-    for(int i=0;i<10;i++){
-        int x=1;//local variable 
-        //cout<<"x is"<<x<<endl; this is correct way of printing local variable
-    }
-    cout<<"x is"<<x<<endl;//we can print local variable like that
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i=0;i<10;i++){
+//         int x=1;//local variable 
+//         //cout<<"x is"<<x<<endl; this is correct way of printing local variable
+//     }
+//     cout<<"x is"<<x<<endl;//we can print local variable like that
+// }
