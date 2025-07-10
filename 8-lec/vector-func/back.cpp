1c0ac9db58c84875a4fcf965a4ab803258546fff
Mohit <mohitsankhyan253@gmail.com>
2025-07-10 19:00:28 +0530

    back

---
+++ 8-lec/vector-func/back.cpp
@@ -0,0 +1,9 @@
+//return back element
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4,5};
+    cout<<"size of vector is "<<vec.back()<<endl;
+}
