786f184ff3de8675fc31d19f69bcce98f9fd4242
Mohit <mohitsankhyan253@gmail.com>
2025-07-10 19:01:41 +0530

    at

---
+++ 8-lec/vector-func/at.cpp
@@ -0,0 +1,9 @@
+//return at index element
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4,5};
+    cout<<"size of vector is "<<vec.at(3)<<endl;
+}
