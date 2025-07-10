b5491c252a90a398bfff6fe1372667d2987a3df8
Mohit <mohitsankhyan253@gmail.com>
2025-07-10 18:57:51 +0530

    pop back

--- 8-lec/vector-func/popBack.cpp
+++ 8-lec/vector-func/popBack.cpp
@@ -7,6 +7,9 @@ int main(){
     vector<int>vec;
     cout<<"size of vector is "<<vec.size()<<endl;
     vec.push_back(25);
+    vec.push_back(50);
+    vec.push_back(75);
+    vec.pop_back();
     cout<<"sizze after push back "<<vec.size()<<endl;
     for(int i:vec){
         cout<<i<<endl;
