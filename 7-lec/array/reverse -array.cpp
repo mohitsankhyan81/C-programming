0529e6dbe51fba736999bdb745060f71345a788a
Mohit <mohitsankhyan253@gmail.com>
2025-07-10 12:15:31 +0530

    reverce of array

---
+++ 7-lec/array/reverse -array.cpp
@@ -0,0 +1,21 @@
+//reverse an array
+
+#include <iostream>
+using namespace std;
+void Rev_arr(int arr[],int size){
+    int start=0,end=size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+    int arr[]={12,3,45,34,23,1,2,7};
+    int size=sizeof(arr)/sizeof(int);
+    Rev_arr(arr,size);
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
