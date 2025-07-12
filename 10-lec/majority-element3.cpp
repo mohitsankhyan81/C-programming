76529e37ef9183866e1ed300d0dc53abfc12eac3
Mohit <mohitsankhyan253@gmail.com>
2025-07-12 13:19:49 +0530

    mories approch

---
+++ 10-lec/majority-element3.cpp
@@ -0,0 +1,28 @@
+//majority element
+//mories approch
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2}; // example input
+    int n = nums.size();
+    int freq = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (freq == 0) {
+            ans = nums[i];
+        }
+        if (ans == nums[i]) {
+            freq++;
+        } else {
+            freq--;
+        }
+    }
+
+    cout << "Majority element is: " << ans << endl;
+    return 0;
+}
