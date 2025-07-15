f6fa80ce1e9da43d1fd20a9f1dd32d801ebb6182
Mohit <mohitsankhyan253@gmail.com>
2025-07-15 12:07:47 +0530

    solved

--- z-py-Q1/Q21.cpp
+++ z-py-Q1/Q21.cpp
@@ -1,1 +1,15 @@
 // WAF to reverse an Integer n.
+
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int n=523;
+    int rev=0;
+    while(n>0){
+        int rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    cout<<rev;
+}
