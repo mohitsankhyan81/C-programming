2f3230f562bf540cd2c7e5143e8db9ce6a568f21
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-02 18:44:35 +0530

    commit

---
+++ 27-lec/pair/pair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include<deque>
+using namespace std;
+int main(){
+  
+  for(int val:d){
+    cout<<val<<" ";
+  }
+  cout<<endl;
+  return 0;
+}
