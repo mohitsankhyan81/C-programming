e2ac36ab88e76626e90fea43de675860fd394f53
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-03 07:52:23 +0530

    commit

---
+++ 27-lec/map/map.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// #include <queue>
+// #include <map>
+// using namespace std;
+
+// int main() {
+//     map<string, int> m;
+//     m["tv"] = 100;
+//     m["laptop"] = 100;
+//     m["headphone"] = 50;
+
+//     for (auto p : m) {
+//         cout << p.first << " " << p.second << endl;
+//     }
+
+//     return 0;
+// }
