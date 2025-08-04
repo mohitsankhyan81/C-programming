53150820f309f6358b20d9545255b358ede5bc65
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-04 11:09:27 +0530

    commit

---
+++ opps/Q7.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+// class name{
+//   private:
+//     int lenth;
+//     int breath;
+//   public:
+//     void setData(int a,int b){
+//       lenth=a;
+//       breath=b;
+//     }
+//     int area(){
+//       return lenth*breath;
+//     }
+//     int perameter(){
+//       return 2*(lenth+breath);
+//     }
+// };
+// int main(){
+//   name obj;
+//   obj.setData(10,10);
+//   cout<<"area is"<<obj.area()<<endl;
+//   cout<<"perameter is"<<obj.perameter()<<endl;
+// }
