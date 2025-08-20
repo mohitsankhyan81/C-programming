58e5576faa935e7c2bb1d4d536211be64e57ee81
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-20 20:15:23 +0530

    commit

---
+++ 35-lec/linear-search.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+
+// bool linearSearch(int matrix[4][3],int row,int col,int key){
+//   for(int i=0;i<row;i++){
+//      for(int j=0;j<col;j++){
+//         if(matrix[i][j]==key){
+//           return true;
+//         }
+//      }
+//   }
+//   return false;
+// }
+
+
+// int main(){
+//   int matrix[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+//   int row=4;
+//   int cols=3;
+//   int key=8;
+//   cout<<linearSearch(matrix,row,cols,key)<<endl;
+// }
