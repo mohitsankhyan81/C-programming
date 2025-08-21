fb5409ae598ff13a30d38e51b737aca7406761d6
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-21 15:12:26 +0530

    commit

---
+++ opps/Q11.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// using namespace std;
+
+// class emp{
+//     int id;
+//     static int count;
+    
+//     public:
+//         void setdata(void){
+//             cout<<"Enter the value of id "<<endl;
+//             cin>>id;
+//             count++;
+//         }
+//         void getdata(void){
+//             cout<<"The id is "<<id<<endl<<"the index is "<<count<<endl;
+//         }
+// };
+// int emp::count;
+// int main(){
+//     emp mohit,sahil,ankit;
+//     mohit.setdata();
+//     mohit.getdata();
+    
+//     sahil.setdata();
+//     sahil.getdata();
+    
+//     ankit.setdata();
+//     ankit.getdata();
+// }
