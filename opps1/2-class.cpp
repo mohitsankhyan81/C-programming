6da8641167f59c38bb7736855404edf499131884
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-27 21:34:55 +0530

    commit

---
+++ opps1/2-class.cpp
@@ -0,0 +1,27 @@
+// //basis
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Teacher{
+//   private:
+//     int salary;
+//   public:
+//     string name;
+//     string dept;
+//     string subject;
+
+//     void setSalary(int s){
+//       salary=s;
+//     }
+//     int getsalary(){
+//       return salary;
+//     }
+// };
+
+// int main(){
+//   Teacher t1;
+//   t1.setSalary(32452);
+//   cout<<t1.getsalary()<<endl;
+// }
