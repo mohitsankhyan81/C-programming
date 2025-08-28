3410b1a5aece392ed2f1208c23091a5469df0aba
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-28 06:48:38 +0530

    commit

---
+++ opps1/5-class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class name{
+  public:
+    int x;
+    static int count;
+  name(){
+    count++;
+  }
+};
+
+int name::count=0;
+int main(){
+  name n1;
+  cout<<name::count<<endl;
+}
