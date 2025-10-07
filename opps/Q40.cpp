90476955527affd82181301390f705c117baae7e
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-10-07 12:48:05 +0530

    commit

---
+++ opps/Q40.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class shape{
+  public:
+    virtual void area(){
+      cout<<"Calculate the area of shape.."<<endl;
+    }
+};
+
+class Circle:public shape{
+  public:
+  void area() override {
+    cout<<"area of Cirle is Pi * r *r ..."<<endl;
+  }
+};
+
+
+class Rectangle:public shape{
+  public:
+    void area() override{
+      cout<<"area of Rectangle: length + breadth"<<endl;
+    }
+};
+int main(){
+  shape *s;
+
+  Circle c;
+  Rectangle r;
+
+  s=&c;
+  s->area();
+
+  s=&r;
+  s->area();
+  
+
+
+  return 0;
+}
