ab30a8b7cca24bc747b5e097b9c01d28cc933ccf
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-10-07 19:57:52 +0530

    commit

---
+++ opps/Q48.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+  private:
+    int real;
+    int imag;
+  public:
+    Complex(){
+      int real=0;
+      int imag=0;
+    }
+    Complex(int r,int i){
+      real=r;
+      imag=i;
+    }
+
+    Complex operator +(const Complex &c){
+      return Complex(real+c.real,imag+c.imag);
+    }
+
+    Complex operator -(const Complex &c){
+      return Complex(real-c.real,imag-c.imag);
+    }
+
+    void display(){
+      if(imag>0){
+        cout<<real<<" + "<<imag<<" i "<<endl;
+      }
+      else{
+        cout<<real<<imag<<"i"<<endl;
+      }
+    }
+};
+int main(){
+  Complex c1(2,5);
+  Complex c2(4,6);
+
+  Complex c3=c1+c2;
+  c3.display();
+
+  Complex c4=c1-c2;
+  c4.display();
+  return 0;
+}
