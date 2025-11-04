108605b462f54714ed589e2acd06975bb94eeadb
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-11-04 11:37:26 +0530

    commit

--- template/temp4.cpp
+++ template/temp4.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
-template <class T1,class T2>
-class Pair{
-  T1 first;
-  T2 second;
+template <class t>
+class sum{
   public:
-  pair(T1 a,T2 b):first(a),second(b);
-  
+  t num1,num2;
+  sum(t a,t b){
+    num1=a;
+    num2=b;
+  }
+
+  t add(){
+    return num1+num2;
+  }
+};
+int main(){
+
+  sum <int> s1(3,6);
+  cout<<"Sum of integer is"<<s1.add()<<endl;
+
+  return 0;
 }
