61a7aaabf4b5a68ea72207674deb2a2a7c491bd9
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-06 20:14:54 +0530

    commit

--- end-term-c++/13matrix.cpp
+++ end-term-c++/13matrix.cpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 int secmin(int arr[],int n){
-  
+  int min=INT_MAX;
+  int secmin=INT_MAX;
+
+  for(int i=0;i<n;i++){
+    if(arr[i]<min){
+      min=secmin;
+      min=arr[i];
+    }
+    else if(arr[i]<secmin && arr[i]!=min){
+      secmin=arr[i];
+    }
+  }
+  return secmin;
 }
 int main(){
   int arr[]={45,7,34,23,46,34,23,12};
