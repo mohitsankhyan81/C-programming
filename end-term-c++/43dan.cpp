1d0862725094da0e4b63f6c7e1dadbdf922ee18c
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-08 18:37:01 +0530

    commit

--- end-term-c++/43dan.cpp
+++ end-term-c++/43dan.cpp
@@ -1,26 +1,36 @@
-//danmic programing 
+// //danmic programing 
 
+// #include <iostream>
+// using namespace std;
+
+// class number{
+//   private:
+//     int x;
+//     int y;
+//   public:
+//     number(int x,int y){
+//       this->x=x;
+//       this->y=y;
+//     }
+
+//     void sum(){
+//       cout<<x+y<<endl;
+//     }
+// };
+// int main(){
+//   number *n;
+//   n=new number(34,45);
+//   n->sum();
+//   delete n;
+//   return 0;
+// }
 #include <iostream>
 using namespace std;
-
-class number{
-  private:
-    int x;
-    int y;
-  public:
-    number(int x,int y){
-      this->x=x;
-      this->y=y;
-    }
-
-    void sum(){
-      cout<<x+y<<endl;
-    }
-};
 int main(){
-  number *n;
-  n=new number(34,45);
-  n->sum();
-  delete n;
-  return 0;
+  for(;;)
+{
+ cout<<"Hi";
+ break;
+}
+
 }
