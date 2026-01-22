5c1b33c99f004409df84e9128461509dcf33fe6f
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-22 21:56:32 +0530

    commit

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,20 +3,24 @@ using namespace std;
 void bubblesort(int arr[],int n){
   for(int i=0;i<n-1;i++){
     for(int j=0;j<n-i-1;j++){
-      if(arr[j]>arr[j+1]){
+      if(arr[j]%2!=0){
+      if(arr[j+1]%2==0){
+      if(arr[j]<arr[j+1]){
         int temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
       }
     }
   }
-
+      }
+    }
   for(int i=0;i<n;i++){
-    cout<<arr[i]<<" ";
+      cout<<arr[i]<<" ";
   }
 }
+
 int main(){
-  int arr[]={45,35,6,34,34,23};
+  int arr[]={4,9,16,12,17,21};
   int size=sizeof(arr)/sizeof(int);
   bubblesort(arr,size);
 }
