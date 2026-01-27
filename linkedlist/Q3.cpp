edea11c6d181032ef6f40783c57d146ba72088a1
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-27 11:14:51 +0530

    commit

---
+++ linkedlist/Q3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int data;
+  Node* next=NULL;
+};
+
+Node* createNode(int val){
+  Node* n=new Node();
+  n->data=val;
+  n->next=NULL;
+  return n;
+}
+
+void insertintoHead(Node*& Head,int val){
+  Node* n=createNode(val);
+  n->next=Head;
+  Head=n;
+}
+
+void insertintoEnd(Node*& Head,int val){
+  Node* n=createNode(val);
+
+  if(Head==NULL){
+    return;
+  }
+
+  Node* temp=Head;
+  while(temp->next!=NULL){
+    temp=temp->next;
+  }
+  temp->next=n;
+}
+
+void deleteintoHead(Node* &Head){
+  if(Head==NULL){
+    return;
+  }
+
+  Node* temp=Head;
+  Head=Head->next;
+  delete temp;
+}
+
+void display(Node* Head){
+  while(Head!=NULL){
+    cout<<Head->data<<" -> ";
+    Head=Head->next;
+  }
+  cout<<"NULL";
+}
+
+int main(){
+  Node* Head=NULL;
+  insertintoHead(Head,45);
+  insertintoHead(Head,67);
+  insertintoEnd(Head,12);
+  insertintoEnd(Head,65);
+  deleteintoHead(Head);
+  display(Head);
+  return 0;
+}
