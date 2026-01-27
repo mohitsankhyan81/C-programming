1ffb2df272e02d1240ca631e87a92b182fa68f09
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-27 21:13:37 +0530

    commit

--- linkedlist/Q3.cpp
+++ linkedlist/Q3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Node{
   int data;
-  Node* next=NULL;
+  Node* next=NULL; 
 };
 
 Node* createNode(int val){
@@ -13,7 +13,7 @@ Node* createNode(int val){
   return n;
 }
 
-void insertintoHead(Node*& Head,int val){
+void insertintoHead(Node* &Head,int val){
   Node* n=createNode(val);
   n->next=Head;
   Head=n;
@@ -33,7 +33,7 @@ void insertintoEnd(Node*& Head,int val){
   temp->next=n;
 }
 
-void deletefromtheHead(Node* &Head){
+void deleteintoHead(Node*& Head){
   if(Head==NULL){
     return;
   }
@@ -43,21 +43,91 @@ void deletefromtheHead(Node* &Head){
   delete temp;
 }
 
+void deleteintoEnd(Node*& Head){
+  if(Head==NULL){
+    return;
+  }
+
+  if(Head->next==NULL){
+    delete Head;
+    Head=NULL;
+    return;
+  }
+
+  Node* temp=Head;
+  while(temp->next->next!=NULL){
+    temp=temp->next;
+  }
+  delete temp->next;
+  temp->next=NULL;
+}
+
+Node* reverse(Node* Head){
+  Node* perv=NULL;
+  Node* curr=Head;
+  Node* next;
+
+  while(curr!=NULL){
+    next=curr->next;
+    curr->next=perv;
+    perv=curr;
+    curr=next;
+  }
+  return perv;
+}
+
+int search(Node* Head,int key){
+  int pos=1;
+  while(Head!=NULL){
+    if(Head->data==key){
+      return pos;
+    }
+    pos++;
+    Head=Head->next;
+  }
+  return -1;
+}
+
+int length(Node* Head){
+  int len=0;
+  while(Head!=NULL){
+    len++;
+    Head=Head->next;
+  }
+  return len;
+}
+
 void display(Node* Head){
   while(Head!=NULL){
     cout<<Head->data<<" -> ";
     Head=Head->next;
   }
-  cout<<"NULL";
+  cout<<"NULL"<<endl;
 }
 
 int main(){
   Node* Head=NULL;
-  insertintoHead(Head,45);
-  insertintoHead(Head,67);
-  insertintoEnd(Head,12);
-  insertintoEnd(Head,65);
-  deletefromtheHead(Head);
+  insertintoHead(Head,34);
+  insertintoHead(Head,46);
+  insertintoEnd(Head,22);
+  insertintoEnd(Head,78);
+  insertintoHead(Head,56);
+  insertintoEnd(Head,11);
+  deleteintoHead(Head);
+  deleteintoEnd(Head);
   display(Head);
+
+  Head=reverse(Head);
+  display(Head);
+
+  int pos=search(Head,46);
+  if(pos!=-1){
+    cout<<"Element at "<<pos<<endl;
+  }
+  else{
+    cout<<"Not Found"<<endl;
+  }
+
+  cout<<"Length of the linked list "<<length(Head)<<endl;
   return 0;
 }
