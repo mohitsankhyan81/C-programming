94abda1820b445b9901949cdcbd8db6acbb8de57
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-05 11:09:09 +0530

    find the missing no

--- 4semcollage/20linerSearch.cpp
+++ 4semcollage/20linerSearch.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+
 using namespace std;
 int main(){
     int arr[]={1,2,4,5,6};
     int n=sizeof(arr)/sizeof(int);
+    int diff=arr[1]-arr[0];
+    for(int i=1;i<n-1;i++){
+        diff=min(diff,arr[i+1]-arr[i]);
+    }
     for(int i=0;i<n-1;i++){
         if(arr[i+1]-arr[i]!=1){
-            cout<<arr[i]+1;
-            break;
+            cout<<arr[i]+diff;
         }
     }
     return 0;
