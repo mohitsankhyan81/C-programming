7df4223b4f2346a7cab629b710d2b3b56f700e0a
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-05 14:53:05 +0530

    secmax

---
+++ 4semcollage/25secodmax.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int SecondMax(int arr[],int n){
+    int max=INT_MIN;
+    int secmax=INT_MIN;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            secmax=max;
+            max=arr[i];
+        }
+        else if(arr[i]>secmax&& arr[i]!=max){
+            secmax=arr[i];
+        }
+    }
+    return secmax;
+}
+
+int main(){
+    int arr[]={45,56,34,23,66};
+    int n=sizeof(arr)/sizeof(int);
+
+    cout<<SecondMax(arr,n);
+    return 0;
+}
