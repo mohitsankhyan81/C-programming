ffd46a132665dcda95b14a250f6cf6955ae4dcc5
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-05 11:28:08 +0530

    quick sort

---
+++ 4semcollage/22quicksort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[],int st,int end){
+    int pivot=arr[end];
+    int idx=st-1;
+    for(int i=st;i<end;i++){
+        if(arr[i]<pivot){
+            idx++;
+            swap(arr[i],arr[idx]);
+        }
+    }
+    idx++;
+    swap(arr[idx],arr[end]);
+    return idx;
+}
+void quicksort(int arr[],int st,int end){
+    if(st<end){
+        int pivot=partition(arr,st,end);
+        quicksort(arr,st,pivot-1);
+        quicksort(arr,pivot+1,end);
+    }
+}
+int main(){
+    int arr[]={34,56,78,11,23};
+    int n=sizeof(arr)/sizeof(int);
+
+    quicksort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
