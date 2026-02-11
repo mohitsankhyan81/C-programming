7969ecf68c897246d29c9dacaa1b1b43db805692
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-11 23:20:15 +0530

    not complete

---
+++ 4semcollage/33reverseoddevenindex.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// using namespace std;
+
+// void reverse(int arr[],int n){
+//     int i=0;
+//     int j=n-1;
+//     while(i<j){
+//         if(i%2!=0&& j%2!=0){
+//             swap(arr[i],arr[j]);
+//         }
+//         i++;
+//         j--;
+//     }
+    
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+// int main(){
+//     int arr[]={1,2,3,4,5,6};
+//     int n=sizeof(arr)/sizeof(int);
+
+//     reverse(arr,n);
+
+//     return 0;
+// }
