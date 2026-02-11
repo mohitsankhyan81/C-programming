d43b8935255a9bf0bc29f98dae25487156b3e99f
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-11 23:26:50 +0530

    commit

---
+++ 4semcollage/34bubblesort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void bubblesort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[]={45,34,76,33,2};
+    int n=sizeof(arr)/sizeof(int);
+
+    bubblesort(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
