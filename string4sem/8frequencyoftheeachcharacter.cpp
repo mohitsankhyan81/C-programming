2f77d725d648af42e0d213b6c7b8ab1a78a0b51e
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-21 22:44:15 +0530

    stirng

--- string4sem/8frequencyoftheeachcharacter.cpp
+++ string4sem/8frequencyoftheeachcharacter.cpp
@@ -12,7 +12,7 @@ void cheakfreq(string s){
 
    for(int i=0;i<26;i++){
         if(freq[i]>0){
-            cout<<"'"<<char(i+'a')<<"' : "<<freq[i]<<endl;
+            cout<<char(i+'a')<<" "<<freq[i]<<endl;
         }
    }
 }
